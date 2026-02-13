e12634310972056beb1eaf2bdb240dbccad03d53
Garrett Geyer (RIT Student) <ggg5483@rit.edu>
2026-02-13 14:50:54 -0500

    ta debug

--- lib/lab4/I2C/i2c.c
+++ lib/lab4/I2C/i2c.c
@@ -20,7 +20,7 @@
 #include "i2c.h"
 
 #ifndef DEBUG
-#define DEBUG true
+#define DEBUG false
 #endif
 
 /**
@@ -30,23 +30,24 @@
 */
 void I2C1_init(uint16_t targetAddress){
 		//if not reset
-	if (I2C1->GPRCM.STAT & I2C_STAT_RESETSTKY_MASK){
+	if (!(I2C1->GPRCM.PWREN & I2C_PWREN_ENABLE_MASK)){
 		//reset
-		I2C1->GPRCM.RSTCTL = I2C_RSTCTL_KEY_UNLOCK_W | I2C_RSTCTL_RESETASSERT_ASSERT;
+		I2C1->GPRCM.RSTCTL = I2C_RSTCTL_KEY_UNLOCK_W | I2C_RSTCTL_RESETASSERT_ASSERT | I2C_RSTCTL_RESETSTKYCLR_CLR;
 		//enable
 		I2C1->GPRCM.PWREN = I2C_PWREN_KEY_UNLOCK_W | I2C_PWREN_ENABLE_ENABLE;
 	} //end if
 	
 	//config SDA, input enabled
-	IOMUX->SECCFG.PINCM[IOMUX_PINCM16] &= ~IOMUX_PINCM_PF_MASK; //clear pf
-	IOMUX->SECCFG.PINCM[IOMUX_PINCM16] = 		IOMUX_PINCM_PC_CONNECTED 
+	//IOMUX->SECCFG.PINCM[IOMUX_PINCM16] &= ~IOMUX_PINCM_PF_MASK; //clear pf
+	IOMUX->SECCFG.PINCM[IOMUX_PINCM16] |= 		IOMUX_PINCM_PC_CONNECTED 
 																				| IOMUX_PINCM16_PF_I2C1_SDA 
 																				| IOMUX_PINCM_INENA_ENABLE;
 	
 	//config SCL
-	IOMUX->SECCFG.PINCM[IOMUX_PINCM15] &= ~IOMUX_PINCM_PF_MASK; //clear pf
-	IOMUX->SECCFG.PINCM[IOMUX_PINCM15] = 		IOMUX_PINCM_PC_CONNECTED
-																				| IOMUX_PINCM15_PF_I2C1_SCL;
+	//IOMUX->SECCFG.PINCM[IOMUX_PINCM15] &= ~IOMUX_PINCM_PF_MASK; //clear pf
+	IOMUX->SECCFG.PINCM[IOMUX_PINCM15] |= 		IOMUX_PINCM_PC_CONNECTED
+																				| IOMUX_PINCM15_PF_I2C1_SCL
+																				| IOMUX_PINCM_INENA_ENABLE;
 	
 	//select BUSCLK
 	I2C1->CLKSEL = I2C_CLKSEL_BUSCLK_SEL_ENABLE;
@@ -56,7 +57,7 @@ void I2C1_init(uint16_t targetAddress){
 	I2C1->CLKDIV = I2C_CLKDIV_RATIO_DIV_BY_1;
 	
 	//disable analog glitch suppresion
-	I2C1->GFCTL &= ~I2C_GFCTL_AGFEN_ENABLE;
+	I2C1->GFCTL &= ~(I2C_GFCTL_AGFEN_ENABLE);
 	
 	//clear controller control register
 	I2C1->MASTER.MCTR = 0;
@@ -64,24 +65,24 @@ void I2C1_init(uint16_t targetAddress){
 	//set clock timer period
 	I2C1->MASTER.MTPR = 0x1F;
 	
+	
 	//RX trigger FIFO contains >= 1 byte
-	I2C1->MASTER.MFIFOCTL &= ~I2C_MFIFOCTL_TXTRIG_MASK ;
+	I2C1->MASTER.MFIFOCTL |= I2C_MFIFOCTL_TXTRIG_EMPTY | I2C_MFIFOCTL_RXTRIG_LEVEL_1;
 	// TX trigger FIFO is empty
-	I2C1->MASTER.MFIFOCTL &= ~I2C_MFIFOCTL_RXTRIG_MASK;
+	
+	//I2C1->MASTER.MFIFOCTL |= ~I2C_MFIFOCTL_RXTRIG_MASK;
 	
 	//disable clock streaching
-	I2C1->MASTER.MCR &= ~I2C_MCR_CLKSTRETCH_MASK;
+	I2C1->MASTER.MCR = ~(I2C_MCR_CLKSTRETCH_MASK);
 	
 	//set target address
-	I2C1->MASTER.MCR &= ~I2C_MSA_SADDR_MASK;
-	I2C1->MASTER.MCR |= ((targetAddress << 1) & I2C_MSA_SADDR_MASK);
+	//I2C1->MASTER.MCR &= ~I2C_MSA_SADDR_MASK;
+	I2C1->MASTER.MCR |= (targetAddress << I2C_MSA_SADDR_OFS);
 	
 	//enable I2C controller
-	I2C1->MASTER.MCTR = I2C_MCR_ACTIVE_ENABLE;
+	I2C1->MASTER.MCTR |= I2C_MCR_ACTIVE_ENABLE;
 	
 	
-	// enable UART enable ENABLE bit of CTL0
-	UART0->CTL0 |= UART_CTL0_ENABLE_ENABLE; 
 	
 }
 
@@ -92,8 +93,8 @@ void I2C1_init(uint16_t targetAddress){
 */
 void I2C1_putchar(unsigned char ch){
 	//wait until tx fifo not full
-	while(((I2C1->MASTER.MFIFOSR & I2C_MFIFOSR_TXFIFOCNT_MASK) >> 8) > 0 ) {
-		#if DEBUG
+	while(!((I2C1->MASTER.MFIFOSR & I2C_MFIFOSR_TXFIFOCNT_MASK) >=  I2C_MFIFOSR_TXFIFOCNT_MINIMUM)) {
+		#if 0
 		volatile int testing = I2C1->MASTER.MFIFOSR;
 		testing = ((I2C1->MASTER.MFIFOSR & I2C_MFIFOSR_TXFIFOCNT_MASK) >> 8);
 		#endif //debug
@@ -110,13 +111,40 @@ void I2C1_putchar(unsigned char ch){
  * @param[in] data_size - Amount of bytes to transmit
 */
 void I2C1_put(unsigned char *data, uint16_t data_size){
-	int i = 0;
-	char ch = data[i++];
-	while(ch != '\0'){
-		I2C1_putchar(ch);
-		ch = data[i++];
-		if (i == data_size){break;}//this may need testing
+	
+	/*
+	set MSA dir transmit
+	set number of bytes for transaction in MCTR
+	start enable and stop enable MCTR and enable burst run.
+	for loop
+		put charectors
+	
+	wait till not idle
+	turn off burst run
+	*/
+	
+	I2C1->MASTER.MSA |= I2C_MSA_DIR_TRANSMIT;
+	
+	
+	//I2C1->MASTER.MCTR &= ~I2C_MCTR_MBLEN_MASK;
+	I2C1->MASTER.MCTR |= ((data_size << I2C_MCTR_MBLEN_OFS) & I2C_MCTR_MBLEN_MAXIMUM);
+	I2C1->MASTER.MCTR |= I2C_MCTR_BURSTRUN_ENABLE;
+	I2C1->MASTER.MCTR |= I2C_MCTR_START_ENABLE | I2C_MCTR_STOP_ENABLE;
+	for(int i = 0; i < data_size; i++){
+		I2C1_putchar(data[i]);
+		
 	}
+	//wait until idle
+	while(!(I2C1->MASTER.MSR & I2C_MSR_IDLE_MASK)){
+	
+	}
+		
+	//turn off burst run
+	I2C1->MASTER.MCTR &= ~I2C_MCTR_BURSTRUN_ENABLE;
+		
+	//direction recieve
+	//I2C1->MASTER.MSA |= I2C_MSA_DIR_MASK;
+	
 }
 
 
