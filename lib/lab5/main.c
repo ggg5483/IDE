01fc1e86cf4645b56d26fd8c1621cdcbef6bec70
Garrett Geyer (RIT Student) <ggg5483@rit.edu>
2026-02-20 15:36:02 -0500

    added a bit to main.c

--- lib/lab5/main.c
+++ lib/lab5/main.c
@@ -13,13 +13,34 @@
 
 #include "timers.h"
 #include "switches.h"
-#include "uart.c"
+//#include "uart.c"
+#include "lab1/leds.h"
+
+
 
 int main(void){
-	
+	S1_init_interrupt();
+	S2_init_interrupt();
+	TIMG0_init(0xFFFF, 0);
+	TIMG6_init(0x00FF, 0);
+	TIMG12_init(0xFFFF);
+	LED1_init();
+	LED2_init();
 
 	
 
 	for(;;){}
 	return 0;
 }
+
+void TIMG0_IRQHandler(void){
+	
+}
+void TIMG6_IRQHandler(void){
+	
+}
+void TIMG12_IRQHandler(void){
+}
+void GROUP1_IRQHandler(void){
+}
+
--- proj/Lab05/Objects/main.d
+++ proj/Lab05/Objects/main.d
@@ -1,2 +1,3 @@
 ./objects/main.o: ../../lib/lab5/main.c ../../lib/lab5/timers.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ../../lib/lab5/switches.h ../../lib\lab1/leds.h
